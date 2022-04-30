2df60cb682b51387c9634b69e9c0d1921482349a
bbeversdorf <bbeversdorf@users.noreply.github.com>
2022-04-29 21:30:45 -0500

    Fixes release build (#2)
    * Update release.yml
    
    * Rename Files

--- build_release.sh
+++ build_release.sh
@@ -3,13 +3,10 @@
 nuget restore
 # Options Debug|Release
 CONFIGURATION=Release
-msbuild ResxTranslator/ResxTranslator.csproj /property:Configuration=$CONFIGURATION
 rm -rf $CONFIGURATION
-
-mkdir -p $CONFIGURATION/lib
-mkdir -p $CONFIGURATION/bin
-mv ResxTranslator/bin/$CONFIGURATION $CONFIGURATION/lib/ResxTranslator
+msbuild /t:ResxTranslator /property:Configuration=$CONFIGURATION /p:OutputPath=../$CONFIGURATION/lib/ResxTranslator
+mkdir -p ./$CONFIGURATION/bin
 echo "#!/bin/sh
 SCRIPT_DIR=\$( cd -- \"\$( dirname -- \"\${BASH_SOURCE[0]}\" )\" &> /dev/null && pwd )
-exec mono \$SCRIPT_DIR/../lib/ResxTranslator/ResxTranslator.exe \"\$@\"" >> $CONFIGURATION/bin/resx_translator
-chmod +x $CONFIGURATION/bin/resx_translator
+exec mono \$SCRIPT_DIR/../lib/ResxTranslator/ResxTranslator.exe \"\$@\"" >> ./$CONFIGURATION/bin/resx_translator
+chmod +x ./$CONFIGURATION/bin/resx_translator
